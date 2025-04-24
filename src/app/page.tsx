d5ccb7eb26a4d1b45c8aaceb0d28282e8c833ef2
tkgin8931 <166148031+tkgin8931@users.noreply.github.com>
2025-04-24 19:36:54 +0900

    ajbbah

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default function Top() {
   const isDesktop = useMediaQuery(768);
   return (
     <div>
-      {isDesktop ? <RetroDesktop /> : <WindowDialog title="このサイトについて" onClose={() => 0} initialPosition={{ x: 0, y: 0 }} />}
+      {isDesktop ? <RetroDesktop /> : <WindowDialog title="このサイトについて" onClose={() => 0} initialPosition={{ x: 0, y: 50 }} />}
     </div>
   )
 }
