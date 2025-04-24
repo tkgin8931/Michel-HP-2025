7ca554ed89a799602493a3493802788e02475441
tkgin8931 <166148031+tkgin8931@users.noreply.github.com>
2025-04-24 19:34:53 +0900

    responsive0424

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
 "use client"
 
 import RetroDesktop from "./components/Retro-desktop"
+import WindowDialog from "./components/WindowDialog";
+import useMediaQuery from "./hooks/useMediaQuery"
 
 export default function Top() {
+  const isDesktop = useMediaQuery(768);
   return (
     <div>
-      <RetroDesktop />
+      {isDesktop ? <RetroDesktop /> : <WindowDialog title="このサイトについて" onClose={() => 0} initialPosition={{ x: 0, y: 0 }} />}
     </div>
   )
 }
